69d26f155de0c8dfefa57bf9049eb16f29fa1735
Shahar Yakir <shahar.yakir@gmail.com>
2022-06-16 16:47:26 +0300

    github link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Button, Chip, IconButton, Toolbar } from "@mui/material";
+import { AppBar, Button, Chip, IconButton, Link, Toolbar } from "@mui/material";
 import { Typography } from "@mui/material";
 import { styled } from "@mui/styles";
 import { Box } from "@mui/system";
@@ -7,6 +7,7 @@ import { useContext } from "react";
 import useConnectionStore from "store/connection-store/useConnectionStore";
 import useMainStore from "store/main-store/useMainStore";
 import BaseButton from "./BaseButton";
+import githubIcon from "../github-mark.svg";
 
 const StyledChip = styled(Chip)({
   width: 200,
@@ -28,15 +29,18 @@ function Navbar() {
   const { disconnect, address, connect } = useConnectionStore();
   const { toggleConnectPopup } = useMainStore();
   const { isSandbox } = useContext(EnvContext);
-  
+
   return (
-    <AppBar component="nav" sx={isSandbox ? {"background": "orange"} : {}}>
+    <AppBar component="nav" sx={isSandbox ? { background: "orange" } : {}}>
       <StyledToolbar>
-        <Box sx={{ display: {  sm: "block" } }}>
+        <Box sx={{ display: { sm: "block" } }}>
           {address ? (
             <Box>
               <StyledChip label={address} />
-              <span style={{marginLeft: 12, cursor: 'pointer'}} onClick={() => disconnect()}>
+              <span
+                style={{ marginLeft: 12, cursor: "pointer" }}
+                onClick={() => disconnect()}
+              >
                 Disconnect
               </span>
             </Box>
@@ -46,6 +50,13 @@ function Navbar() {
             </BaseButton>
           )}
         </Box>
+        <Link
+          href="https://github.com/ton-defi-org/jetton-deployer-contracts"
+          target="_blank"
+          sx={{marginLeft: 2}}
+        >
+          <img src={githubIcon} />
+        </Link>
       </StyledToolbar>
     </AppBar>
   );

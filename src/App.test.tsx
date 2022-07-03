4d1e7f930c9760b848403e48e3c67a52d49c28b9
Shahar Yakir <shahar.yakir@gmail.com>
2022-07-03 14:00:34 +0300

    fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import {
   buildJettonOnchainMetadata,
   readJettonMetadata,
 } from "lib/jetton-minter";
-import { beginCell } from "ton";
+import { beginCell, Cell } from "ton";
 import axios from "axios";
 import { persistenceType } from "./lib/jetton-minter";
 
@@ -24,6 +24,7 @@ test("Long serialization", async () => {
   expect(await readJettonMetadata(buildJettonOnchainMetadata(data))).toEqual({
     persistenceType: "onchain",
     metadata: data,
+    isJettonDeployerFaultyOnChainData: false,
   });
 });
 
@@ -32,6 +33,22 @@ test("Short serialization", async () => {
   expect(await readJettonMetadata(buildJettonOnchainMetadata(data))).toEqual({
     persistenceType: "onchain",
     metadata: data,
+    isJettonDeployerFaultyOnChainData: false,
+  });
+});
+
+test("Faulty serialization", async () => {
+  const data = { image: "nope" };
+  
+  // Precomputed with faulty code prior to https://github.com/ton-defi-org/jetton-deployer-webclient/pull/61
+  const faultyContentCell = Cell.fromBoc(
+    "b5ee9c72c1010201002e000005010300c001004da00c20bad98ed5e80064bd29ab119ca237cb7fb76e7686fb8a3d948722faf487c7a00dcdee0cb04871a9c9"
+  )[0];
+
+  expect(await readJettonMetadata(faultyContentCell)).toEqual({
+    persistenceType: "onchain",
+    metadata: data,
+    isJettonDeployerFaultyOnChainData: true,
   });
 });
 

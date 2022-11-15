0dc488e9fb6a39fc0c1d5fb500201c164c0f00db
antongotech <105588893+antongotech@users.noreply.github.com>
2022-11-15 12:11:44 +0200

    Contributed by footer (#124)
    * contributed block added
    
    * contributed block added
    
    * contributed block added
    
    * contributed block added
    
    * contributed block modified
    
    * contributed block modified
    
    * link modified

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { Box, Link, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { HoverableIcon } from "components/hoverableIcon/HoverableIcon";
 import {
+  ContributedWrapper,
   CredentialsWrapper,
+  FooterContributedText,
+  FooterLink,
+  FooterTextBoxLeft,
+  FooterTextBoxRight,
   FooterWrapper,
   Separator,
   SocialsContent,
@@ -13,6 +18,7 @@ import telegramHovered from "assets/icons/telegram-hover.svg";
 import github from "assets/icons/github.svg";
 import githubHovered from "assets/icons/github-hover.svg";
 import { AppLogo } from "components/appLogo";
+import heart from "assets/icons/heart.svg";
 
 export const Footer = () => {
   return (
@@ -36,13 +42,24 @@ export const Footer = () => {
       </SocialsWrapper>
       <Separator />
       <CredentialsWrapper>
-        <Typography variant="body2">© 2022 TON Foundation</Typography>
-        <Link
-          target="_blank"
-          href="https://t.me/+YDnoBue1Dz81ZTMy"
-          sx={{ color: "inherit", textDecoration: "none" }}>
-          <Typography variant="body2">Support</Typography>
-        </Link>
+        <FooterTextBoxLeft>
+          <Typography variant="body2">© 2022 TON Foundation</Typography>
+        </FooterTextBoxLeft>
+        <ContributedWrapper>
+          <FooterContributedText variant="body2">
+            Contributed with {` `}
+            <img src={heart} alt="Orbs logo" width={9} height={7} />
+            {` `} by {` `}
+            <FooterLink sx={{ color: "#CF84D1" }} target="_blank" href="https://orbs.com/">
+              Orbs
+            </FooterLink>
+          </FooterContributedText>
+        </ContributedWrapper>
+        <FooterTextBoxRight>
+          <FooterLink target="_blank" href="https://t.me/+YDnoBue1Dz81ZTMy">
+            <Typography variant="body2">Support</Typography>
+          </FooterLink>
+        </FooterTextBoxRight>
       </CredentialsWrapper>
     </FooterWrapper>
   );
--- src/components/footer/styled.tsx
+++ src/components/footer/styled.tsx
@@ -1,16 +1,16 @@
-import { Box, styled } from "@mui/material";
+import { Box, Link, styled, Typography } from "@mui/material";
 import { APP_GRID } from "consts";
 
-const FooterWrapper = styled(Box)(() => ({
+const FooterWrapper = styled(Box)({
   maxWidth: APP_GRID,
   width: "calc(100% - 50px)",
   margin: "auto",
-}));
+});
 
-const SocialsWrapper = styled(Box)(() => ({
+const SocialsWrapper = styled(Box)({
   display: "flex",
   justifyContent: "space-between",
-}));
+});
 
 const SocialsContent = styled(Box)(({ theme }) => ({
   display: "flex",
@@ -21,17 +21,93 @@ const SocialsContent = styled(Box)(({ theme }) => ({
 
 const CredentialsWrapper = styled(Box)(({ theme }) => ({
   display: "flex",
+  flexWrap: "wrap",
   justifyContent: "space-between",
   marginTop: theme.spacing(2),
   marginBottom: theme.spacing(2),
   color: "#728A96",
   fontSize: 14,
+  [theme.breakpoints.down("md")]: {
+    "& > *": {
+      marginBottom: `${theme.spacing(1)} !important`,
+    },
+  },
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+  },
 }));
 
-const Separator = styled("hr")(() => ({
+const Separator = styled("hr")({
   height: "1px",
   backgroundColor: "#e6e6e6",
   border: "none",
+});
+
+const FooterLink = styled(Link)(({ theme }) => ({
+  display: "inline-flex",
+  alignItems: "center",
+  color: "inherit",
+  textDecoration: "none",
+  [theme.breakpoints.down("md")]: {
+    justifyContent: "flex-end",
+  },
 }));
 
-export { FooterWrapper, SocialsWrapper, SocialsContent, Separator, CredentialsWrapper };
+const CenteringWrapper = styled(Box)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const FooterContributedText = styled(Typography)({
+  fontSize: 12,
+});
+
+const ContributedWrapper = styled(CenteringWrapper)(({ theme }) => ({
+  [theme.breakpoints.down("md")]: {
+    minWidth: "100%",
+    flex: 2,
+    order: 3,
+  },
+}));
+
+const FooterTextBox = styled(CenteringWrapper)(({ theme }) => ({
+  [theme.breakpoints.down("md")]: {
+    minWidth: "50%",
+  },
+}));
+
+const FooterTextBoxLeft = styled(FooterTextBox)(({ theme }) => ({
+  [theme.breakpoints.down("md")]: {
+    justifyContent: "start",
+  },
+  [theme.breakpoints.down("sm")]: {
+    justifyContent: "center",
+    minWidth: "100%",
+  },
+}));
+
+const FooterTextBoxRight = styled(FooterTextBox)(({ theme }) => ({
+  [theme.breakpoints.down("md")]: {
+    justifyContent: "end",
+  },
+  [theme.breakpoints.down("sm")]: {
+    justifyContent: "center",
+    minWidth: "100%",
+  },
+}));
+
+export {
+  FooterWrapper,
+  SocialsWrapper,
+  SocialsContent,
+  Separator,
+  CredentialsWrapper,
+  FooterLink,
+  CenteringWrapper,
+  FooterContributedText,
+  ContributedWrapper,
+  FooterTextBox,
+  FooterTextBoxLeft,
+  FooterTextBoxRight,
+};

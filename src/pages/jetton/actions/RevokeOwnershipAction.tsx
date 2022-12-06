4c94d7e036165debd4047fa4d81c542ed4632735
antongotech <105588893+antongotech@users.noreply.github.com>
2022-12-06 13:32:43 +0200

    wip

--- src/pages/jetton/actions/RevokeOwnershipAction.tsx
+++ src/pages/jetton/actions/RevokeOwnershipAction.tsx
@@ -6,10 +6,16 @@ import { Address } from "ton";
 import { AppButton } from "components/appButton";
 import { useSetRecoilState } from "recoil";
 import { jettonActionsState } from "pages/jetton/actions/jettonActions";
+import { useState } from "react";
+import { CenteringWrapper } from "components/footer/styled";
+import { Popup } from "components/Popup";
+import { Link, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 function RevokeOwnershipAction() {
   const setActionInProgress = useSetRecoilState(jettonActionsState);
-  const { jettonMaster, isAdmin, getJettonDetails, isMyWallet } = useJettonStore();
+  const [showAlert, setShowAlert] = useState(false);
+  const { jettonMaster, isAdmin, getJettonDetails, isMyWallet, symbol } = useJettonStore();
   const { showNotification } = useNotification();
 
   if (!isAdmin || !isMyWallet) {
@@ -17,6 +23,11 @@ function RevokeOwnershipAction() {
   }
 
   const onClick = async () => {
+    setShowAlert(true);
+  };
+
+  const onSubmit = async () => {
+    setShowAlert(false);
     try {
       if (!jettonMaster) {
         return;
@@ -39,6 +50,42 @@ function RevokeOwnershipAction() {
 
   return (
     <>
+      <Popup open={showAlert} maxWidth="80%" onClose={() => setShowAlert(false)}>
+        <Typography className="title">Revoke Ownership</Typography>
+        <Typography>
+          This operation will revoke your admin rights of the token <b>{symbol}</b>. This means you
+          will not be able to:
+        </Typography>
+        <ul
+          style={{
+            fontSize: 14,
+          }}>
+          <li>Change your logo URL</li>
+          <li>
+            Mint more <b>{symbol}</b>
+          </li>
+          <li>Change the token's name</li>
+          <li>Change the token's description</li>
+          <li>
+            <Link
+              sx={{ textDecoration: "none" }}
+              href="https://github.com/ton-blockchain/minter-contract#jetton-metadata-field-best-practices">
+              Read more
+            </Link>
+          </li>
+        </ul>
+        <Box sx={{ alignSelf: "baseline" }}>
+          <Typography textAlign="left">
+            You should revoke ownership only after all metadata has been finalized.
+          </Typography>
+        </Box>
+        <CenteringWrapper sx={{ alignSelf: "end" }}>
+          <Box mr={1}>
+            <AppButton onClick={() => setShowAlert(false)}>Cancel</AppButton>
+          </Box>
+          <AppButton onClick={onSubmit}>Submit</AppButton>
+        </CenteringWrapper>
+      </Popup>
       <AppButton transparent={true} onClick={onClick}>
         Revoke ownership
       </AppButton>
